5f8abf7d9a71c3b66f189434e9d3f6b327578660
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-09-11 15:52:16 -0500

    installed mongoDB and create user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator(validEmail) {
+        return /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z]{2,6})(\.[a-z]{2,6})?$/.test(
+          validEmail
+        );
+      },
+    },
+  },
+  thoughts: {
+    type: Schema.Types.ObjectId,
+    ref: "Thought",
+  },
+  friends: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+//virtual to provide friend count
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+//create user model using userschema
+const User = model("User", UserSchema);
+
+//export the User model
+module.exports = User;
