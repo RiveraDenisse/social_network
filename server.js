e4552a8041fadc809f65f04fcab67e9d5756aa2c
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-09-11 16:13:52 -0500

    created server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(require("./routes"));
+//mongoose.connect() tells mongoose which database we want to connect to
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social_network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
