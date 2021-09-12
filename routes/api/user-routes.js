b9e1d017bb89da209fd80513e92041fa043459a7
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-09-11 23:11:44 -0500

    add friend to user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,6 +5,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  createFriend,
+  deleteFriend,
 } = require("../../controllers/user-controller");
 // Set up GET all and POST at /api/users
 router.route("/").get(getAllUsers).post(createUser);
@@ -12,4 +14,8 @@ router.route("/").get(getAllUsers).post(createUser);
 // Set up GET one, PUT, and DELETE at /api/user/:id
 router.route("/:userId").get(getUserbyId).put(updateUser).delete(deleteUser);
 
+router
+  .route("/:userId/friends/:friendId")
+  .post(createFriend)
+  .delete(deleteFriend);
 module.exports = router;
